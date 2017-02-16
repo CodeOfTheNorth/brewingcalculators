9c1f11881e3bcea3b02e08e52690c37912f46f38
Gabe Masiulis <gabe@masiul.is>
2017-02-15 23:02:18 -0600

    :lipstick: that was bothering me. Weclome dialog fixed.

--- src/app/dialogs/welcome-dialog.component.ts
+++ src/app/dialogs/welcome-dialog.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'welcome-dialog',
+  selector: 'md-card',
   // template: '<md-dialog-content color="primary"><h1>Hi!</h1></md-dialog-content>'
   templateUrl: './welcome-dialog.component.html'
 })
