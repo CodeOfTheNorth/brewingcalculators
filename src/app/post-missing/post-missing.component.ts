0ac9395136cc5bc5ed1ec40eacd1a6b28db09ddf
Gabe Masiulis <gabe@masiul.is>
2017-02-21 21:29:04 -0600

    :construction: added a post method... and it doesn't work. Debugging needed.

--- src/app/post-missing/post-missing.component.ts
+++ src/app/post-missing/post-missing.component.ts
@@ -7,9 +7,23 @@ import { CalcsService } from '../calcs.service';
   styleUrls: ['./post-missing.component.css']
 })
 export class PostMissingComponent implements OnInit {
-
+  body:any[];
+  keyword:string;
+  name:string;
+  description:string;
   constructor(private _calcsService: CalcsService) { }
+  sendKeyword(){
+    console.log(this.keyword);
+    this.body = [];
+    this.body["keyword"] = this.keyword;
+    this.body["user_info"] = this.name;
+    this.body["description"] = this.description;
+    console.log(this.body);
+    this._calcsService.postMissing(this.body)
+      .subscribe(res => {
 
+      });
+  }
   ngOnInit() {
   }
 
