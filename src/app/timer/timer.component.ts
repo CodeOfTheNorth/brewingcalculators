8c107a13024eb0e8b0ccbd75cd0d66891ae60fc8
Gabe Masiulis <gabe@masiul.is>
2017-02-23 21:03:58 -0600

    :construction: added input field for timer. Validating the field the hard way.

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -6,10 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timer.component.css']
 })
 export class TimerComponent implements OnInit {
-
+  timerInput:string;
+  brewtimer:any;
   constructor() { }
 
-  ngOnInit() {
+  validateTimerInput(){
+    // keyup event could be a backspace leaving us with an empty string.
+    if (this.timerInput.length == 0){
+      return
+    } else if (this.timerInput.substring(this.timerInput.length - 1) == ':')
+    // if a colon is the last character entered
+    {
+      if (this.timerInput.length == 1)
+      // if the colon is the first character
+      {
+        // populate hours as zero
+        this.timerInput = '00:';
+        return
+      } else if (this.timerInput.length == 2)
+      // if the colon is the second character
+      {
+        // add a trailing zero
+        this.timerInput = '0' + this.timerInput + ':';
+      } else if (this.timerInput.length > 3)
+      // if the colon is not the third character
+      {
+        // remove the colon
+        this.timerInput = this.timerInput.substring(0,this.timerInput.length-1)
+      }
+    }
   }
 
+  ngOnInit() {
+    if(localStorage['brewtimer']){
+      this.brewtimer = JSON.stringify(localStorage['brewtimer']);
+    } else {
+      localStorage['brewtimer'] = '';
+    }
+  }
 }
