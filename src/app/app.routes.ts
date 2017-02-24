d5f3e4367cb3d346a05931a1a5c6ef4a5b4f623d
Gabe Masiulis <gabe@masiul.is>
2017-02-23 18:10:35 -0600

    :sparkles: using md-chips as routerLinks to different calculators with an ngFor loop.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { TimerComponent } from './timer/timer.component';
 
 export const router: Routes = [
-  { path: '1', component: TimerComponent }
+  { path: '2', component: TimerComponent }
 ]
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(router);
