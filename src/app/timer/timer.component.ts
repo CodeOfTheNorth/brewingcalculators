e37f38417d07a5ffa3700902626d9505f804a910
Gabe Masiulis <gabe@masiul.is>
2017-02-25 19:24:37 -0600

    shitty timer works

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -8,19 +8,72 @@ import { Component, OnInit } from '@angular/core';
 export class TimerComponent implements OnInit {
   timerInput:string;
   brewtimer:any;
+  timeRemaining:string;
+  alarmTime:string;
+  // timeLocaleEnd:string;
+  timeEnd: number;
   constructor() { }
 
   setTimer(){
     if (this.timerInput){
-      if(this.timerInput.match(/\d{2}:\d{2}$/)){
+      if(this.timerInput.match(/\d{2}:\d{2}$/))
+      // make sure the timer input matches our validation
+      {
+        this.timeRemaining = this.timerInput;
+        // bind it to the display
         var hours = parseInt(this.timerInput.substring(0,2));
         var minutes = parseInt(this.timerInput.substring(3,5));
+        // pull the hours and minutes from the plain text string
         var milliseconds = (hours * 60 + minutes) * 60000;
-        console.log(new Date(milliseconds).toTimeString());
-        console.log(new Date(milliseconds + Date.now()).toLocaleTimeString());
+        // convert the total amount into milliseconds
+
+        console.log(Date.now());
+        // now in milliseconds
+        console.log(Date.now() + milliseconds);
+        // timer end time in milliseconds
+
+        this.timeEnd = milliseconds + Date.now();
+        localStorage['brewtimer'] = this.timeEnd.toString();
+        this.alarmTime = new Date(this.timeEnd).toTimeString();
+
+        // this.timeEnd = new Date(milliseconds + Date.now());
+        // bind timeEnd to the date object of the ending time
+
+        // this.timeLocaleEnd = new Date(milliseconds + Date.now()).toLocaleTimeString();
+        // bind timeLocaleEnd to a string of the local time when the calculator ends
+
+        // Date.UTC(year, month, day, hours, minutes, seconds, millisec)
+        // console.log(new Date(Date.UTC(0,0,0,hours,minutes)));
+        // console.log(new Date(milliseconds));
+
+        // console.log(new Date(milliseconds).toTimeString());
+        // console.log(typeof new Date(milliseconds).toTimeString());
+        // console.log(this.timeLocaleEnd);
+        // console.log(this.timeLocaleEnd);
+        // console.log(this.timeEnd);
+        // unfinished stuff
+        // console.log(Date.parse(this.timeLocaleEnd));
+        setInterval(() =>{this.runTimer()},1000);
+        // do the thing
       }
     }
-
+  }
+  runTimer(){
+    if (this.timeEnd){
+      this.timeRemaining = '';
+      var remaining = this.timeEnd - Date.now();
+      if (remaining <=0){clearInterval};
+      if (remaining >= 3600000){
+        var hours = Math.floor(remaining / 3600000);
+        var remaining = remaining % 3600000;
+        this.timeRemaining = hours + ':';
+      }
+      var minutes = Math.floor(remaining / 60000);
+      this.timeRemaining += minutes + ':';
+      var remaining = remaining % 60000;
+      var seconds = Math.floor(remaining / 1000);
+      this.timeRemaining += seconds;
+    }
   }
   validateTimerInput(){
     if (this.timerInput.length >= 5 && !this.timerInput.match(/\d{2}:\d{2}$/)){
